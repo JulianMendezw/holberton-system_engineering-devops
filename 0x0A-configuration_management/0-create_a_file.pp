d1e5cb720c2f801323ab7afe526b2fcabe968d30
Julianmendezw <julianlx@hotmail.com>
2021-04-01 17:26:54 -0500

    adding permisions to the files

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet, create a file in /tmp.
+
+file { '/tmp/holberton':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+    }
