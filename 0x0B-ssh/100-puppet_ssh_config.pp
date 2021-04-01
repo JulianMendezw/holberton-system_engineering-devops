b71c19a0da7df9eeaec080a5e60f01cad0557cb6
Julianmendezw <julianlx@hotmail.com>
2021-04-01 17:25:33 -0500

    Adding Lines to /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Adding Lines to /etc/ssh/ssh_config
-file_line { 'ssh_config':
+file_line { 'ssh_config adding IdentityFile':
   ensure => 'present',
   line   => 'IdentityFile ~/.ssh/holberton',
   path   => '/etc/ssh/ssh_config',
 }->
-file_line { 'ssh_config':
+file_line { 'ssh_config adding PasswordAuthentication':
   ensure => 'present',
   line   => 'PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
