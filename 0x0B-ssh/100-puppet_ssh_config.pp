ca17b7f008ef47b9c4d282527f4c22335f1e181c
Julianmendezw <julianlx@hotmail.com>
2021-04-03 13:19:31 -0500

    fixing script 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@ file_line { 'ssh_config adding IdentityFile':
   ensure => 'present',
   line   => 'IdentityFile ~/.ssh/holberton',
   path   => '/etc/ssh/ssh_config',
-}->
+}
 file_line { 'ssh_config adding PasswordAuthentication':
   ensure => 'present',
   line   => 'PasswordAuthentication no',
