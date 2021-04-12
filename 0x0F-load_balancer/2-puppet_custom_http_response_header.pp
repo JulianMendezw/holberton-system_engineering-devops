7bc2427f7f47201010417873f4316b7a2b8eba17
Julian <julianlx@hotmail.com>
2021-04-12 18:49:14 -0500

    append

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,11 +7,7 @@ exec { 'exec_1':
   command => 'sudo apt-get install nginx -y',
 }
 
-exec { 'exec_2':
-  command => 'echo "Holberton School" > /var/www/html/index.nginx-debian.html',
-}
-
--> file_line { 'add header' :
+file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${hostname};",
