64f0c576c4d0452baa3971dcad65d3c4cf40c21c
Julian <julianlx@hotmail.com>
2021-05-17 14:21:28 -0500

    fixing status code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,11 +12,15 @@ def number_of_subscribers(subreddit):
     headers = {'user-agent': 'my-app/0.0.1'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
-    response = get(url, headers=headers, allow_redirects=False).json()
+    response = get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    response = response.json()
 
     if response:
         data = response.get('data')
-        print(data[5])
 
         if data:
             return data.get('subscribers')
