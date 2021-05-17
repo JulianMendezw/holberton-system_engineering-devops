6558686edbc4b493bfa991286f8aca547c101c49
Julian <julianlx@hotmail.com>
2021-05-17 14:28:40 -0500

    getting 10 hot post

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,15 +11,19 @@ def top_ten(subreddit):
     headers = {'user-agent': 'my-app/0.0.1'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
-    response = get(url, headers=headers, allow_redirects=False).json()
+    response = get(url, headers=headers, allow_redirects=False)
 
-    if response:
+    if response.status_code != 200:
+        return 0
+
+    try:
+        response = response.json()
+
         data = response.get('data')
         childrenList = data.get('children')
 
-        hotPostList = []
         for children in childrenList[:10]:
             childrenObj = children.get('data')
             print(childrenObj.get('title'))
-
-    return 0
+    except(BaseException):
+        print(None)
