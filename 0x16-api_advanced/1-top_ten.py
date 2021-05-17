796a529babf3cba416f0fbc6acf1bda8d3fd5f6d
Julian <julianlx@hotmail.com>
2021-05-17 13:53:46 -0500

    test

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+"""
+
+
+def top_ten(subreddit):
+    """ Return the 10 hot posts listed for a given subreddit """
+    from requests import get
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    response = get(url, headers=headers, allow_redirects=False).json()
+
+    if response:
+        data = response.get('data')
+        childrenList = data.get('children')
+
+        hotPostList = []
+        for children in childrenList[:10]:
+            childrenObj = children.get('data')
+            print(childrenObj.get('title'))
+
+    return 0
